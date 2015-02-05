94a5f955dcdb5ef3bece9ad400694cee055bc147
Cami Rodriguez <camiepisode@outlook.com>
2015-02-05 17:09:06 -0500

    Added an animation
    It is for explain how to works Octopus

---
+++ js/animations.js
@@ -0,0 +1,29 @@
+$(function () {
+
+	var element = null;
+	var videoDev = document.getElementById("developerAnimationVideo");
+	var videoCon = document.getElementById("consumerAnimationVideo");
+
+	$("#spinnerBox").hide();
+
+	$("#developerAnimationButton").click(function(){
+		// Developer Animation
+			$("#developerAnimationBox").show();
+			$("consumerAnimationBox").hide();
+			
+			videoCon.pause();
+			videoDev.currentTime = 0;
+			videoDev.play();
+	});
+
+	$("#consumerAnimationButton").click(function(){
+		// Consumer Animation
+			$("developerAnimationBox").hide();
+			$("consumerAnimationBox").show();
+
+			videoDev.pause();
+			videoCon.currentTime = 0;
+			videoCon.play();
+	});
+
+});
