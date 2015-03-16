35516c079eb28740592409c2a89fcf9710a4e8af
Cami Rodriguez <camiepisode@outlook.com>
2015-03-15 21:40:34 -0500

    The consumer video is finished

--- js/animations.js
+++ js/animations.js
@@ -10,19 +10,20 @@ $(function () {
 	$("#developerAnimationButton").click(function(){
 		// Developer Animation
 			$("#developerAnimationBox").show();
-			$("consumerAnimationBox").hide();
+			$("#consumerAnimationBox").hide();
 			
 			videoCon.pause();
+			videoCon.currentTime = 0;
 			videoDev.currentTime = 0;
 			videoDev.play();
 	});
 
 	$("#consumerAnimationButton").click(function(){
 		// Consumer Animation
-			$("developerAnimationBox").hide();
-			$("consumerAnimationBox").show();
-
+			$("#developerAnimationBox").hide();
+			$("#consumerAnimationBox").show();
 			videoDev.pause();
+			videoDev.currentTime = 0;
 			videoCon.currentTime = 0;
 			videoCon.play();
 	});
